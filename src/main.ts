b3194b3824d42e9e81ebd9dbd75dfe26379d5128
Freddy990117 <Freddy990117@gmail.com>
2025-11-28 06:37:59 +0800

    1.Submite 原本為 input 屬性將其更改為 button 2.在 TS 中取得 button

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
-interface Person {
-  Number: number;
-  Name: string;
-  Job: string;
-  Email: string;
-}
-const personNumber = document.getElementById("phone") as HTMLInputElement;
-const personName = document.getElementById("name") as HTMLInputElement;
-const personJob = document.getElementById("job") as HTMLInputElement;
-const personEmail = document.getElementById("email") as HTMLInputElement;
+// 定義名為 Person 的物件的形狀
+// interface Person {
+//   Phone: number;
+//   Name: string;
+//   Job: string;
+//   Email: string;
+// }
 
-const Person1: Person = {
-  Number: Number(personNumber),
-  Name: personName.value,
-  Job: personJob.value,
-  Email: personEmail.value,
-};
+const submitBtn = document.getElementById("submit-btn") as HTMLButtonElement;
+console.log(submitBtn);
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("click btn");
+});
+// // 透過 id 取得 HTML 中的值
+// const personPhone = document.getElementById(".phone") as HTMLInputElement;
+// const personName = document.getElementById("name") as HTMLInputElement;
+// const personJob = document.getElementById("job") as HTMLInputElement;
+// const personEmail = document.getElementById("email") as HTMLInputElement;
 
-console.log(Person1);
+// const Person1: Person = {
+//   Phone: Number(personPhone),
+//   Name: personName.value,
+//   Job: personJob.value,
+//   Email: personEmail.value,
+// };
+
+// console.log(Person1.Phone);
