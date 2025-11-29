512227b505a800f106406e47b2dae764df581337
Freddy <freddy990117@gmail.com>
2025-11-30 07:48:00 +0800

    在 TS 中取得 Input value

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@
 //   Name: string;
 //   Job: string;
 //   Email: string;
+//   Message: string;
 // }
 
 const submitBtn = document.getElementById("submit-btn") as HTMLButtonElement;
-console.log(submitBtn);
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   console.log("click btn");
@@ -18,9 +18,13 @@ submitBtn.addEventListener("click", (e) => {
     if (box.checked) {
       console.log(box.name);
     }
+    const personMessage = document.getElementById(
+      "message"
+    ) as HTMLInputElement;
+    console.log(personMessage.value);
   });
 });
-// // 透過 id 取得 HTML 中的值
+// 透過 id 取得 HTML 中的值
 // const personPhone = document.getElementById(".phone") as HTMLInputElement;
 // const personName = document.getElementById("name") as HTMLInputElement;
 // const personJob = document.getElementById("job") as HTMLInputElement;
