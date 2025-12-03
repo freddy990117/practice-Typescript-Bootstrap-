8568775111f7e644eb4f931caac365e20b4cce61
Freddy990117 <Freddy990117@gmail.com>
2025-12-03 15:22:21 +0800

    1.建立一個 fn 用於存放新的 Person 資訊 2.測試印出確認效果相同

--- src/main.ts
+++ src/main.ts
@@ -12,45 +12,26 @@ const personPhone = document.getElementById("phone") as HTMLInputElement;
 const personName = document.getElementById("name") as HTMLInputElement;
 const personJob = document.getElementById("job") as HTMLInputElement;
 const personEmail = document.getElementById("email") as HTMLInputElement;
-// 取得按鈕
-const submitBtn = document.getElementById("submit-btn") as HTMLButtonElement;
-// 設定參數
-const checkboxArr: Array<string> = [];
+// 取得 Textarea 輸入框
+const personMessage = document.getElementById("message") as HTMLTextAreaElement;
+// 取得所有的 checkbox (是一組的 Nodelist 節點)
+const chechBoxes =
+  document.querySelectorAll<HTMLInputElement>(".form-check-input");
 // 需要重新賦值所以使用 let
 let messageValue: string = "";
 
+// 取得按鈕
+const submitBtn = document.getElementById("submit-btn") as HTMLButtonElement;
 // 按下送出表單會發生的行為
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  // 取得 Textarea 輸入框
-  const personMessage = document.getElementById(
-    "message"
-  ) as HTMLTextAreaElement;
-  // 取得所有的 checkbox (是一組的 Nodelist 節點)
-  const chechBoxes =
-    document.querySelectorAll<HTMLInputElement>(".form-check-input");
-  //  跑迴圈遍布 checkbox
-  chechBoxes.forEach((box) => {
-    // 是否有 check
-    if (box?.checked) {
-      // 將有勾選的選項 push 到 array 當中
-      checkboxArr.push(box.name);
-    }
-  });
+
+  // 將 Person fn 放置此處並列印確認相同
+  const person = newPerson();
   // 賦值給 messageValue
   messageValue = personMessage.value;
 
-  // !先放置在裡面印出，後續放置的位置會再變更
-  const Person1: Person = {
-    Phone: Number(personPhone.value),
-    Name: personName.value,
-    Job: personJob.value,
-    Email: personEmail.value,
-    Message: messageValue,
-    Service: checkboxArr,
-  };
-
-  console.log(Person1);
+  console.log(person);
   const collapseButton = document.getElementById(
     "collapse-button"
   ) as HTMLButtonElement;
@@ -58,17 +39,41 @@ submitBtn.addEventListener("click", (e) => {
     "collapse-content"
   ) as HTMLDivElement;
   // 更改文字顯示使用 innerText
-  collapseButton.innerText = `Request Form : ${Person1.Name}`;
+  collapseButton.innerText = `Request Form : ${person.Name}`;
   // 該改結構使用 innerHTML
   collapseContent.innerHTML = `
-  <h4>Name : ${Person1.Name} </h4>
-  <h4>Phone Number : ${Person1.Phone} </h4>
-  <h4>Job Description : ${Person1.Job} </h4>
-  <h4>Email : ${Person1.Email} </h4>
-  <h4>Service :<br/><h5>${Person1.Service}</h5> </h4>
-  <p>User want talk us : <strong>${Person1.Message} </strong></p>
+  <h4>Name : ${person.Name} </h4>
+  <h4>Phone Number : ${person.Phone} </h4>
+  <h4>Job Description : ${person.Job} </h4>
+  <h4>Email : ${person.Email} </h4>
+  <h4>Service :<br/><h5>${person.Service}</h5> </h4>
+  <p>User want talk us : <strong>${person.Message} </strong></p>
   `;
 });
+
+// 建立一個 Person fn 要符合 Person Interface
+const newPerson = function (): Person {
+  const serviceArr: string[] = [];
+  // 設定參數 (原本全域變數更改為區域變數)
+  const checkboxArr: Array<string> = [];
+
+  chechBoxes.forEach((box) => {
+    // 是否有 check
+    if (box?.checked) {
+      // 將有勾選的選項 push 到 array 當中
+      checkboxArr.push(box.name);
+    }
+  });
+
+  return {
+    Phone: Number(personPhone.value),
+    Name: personName.value,
+    Job: personJob.value,
+    Email: personEmail.value,
+    Message: personMessage.value,
+    Service: checkboxArr,
+  };
+};
 
 // //TODO 按下 submit-form 後，一個行為是將資料放入表單中（已完成，但是後續結構需要更改為每個使用者為新的 According Item），一個行為是 依照 id 新增 According Item，而這樣我需要：
 // 1.建立一個 fn 用於存放新的 Person 資訊（要符合 Person 的 interface 結構）
